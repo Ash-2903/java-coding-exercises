2bbe20e5f5f382fded84d903c57ca1e58d9c36de
Ash-2903 <ashrithaherle2903@gmail.com>
2023-05-22 17:28:28 +0530

    trial commit

--- PositiveNegativeorZero.java
+++ PositiveNegativeorZero.java
@@ -1,3 +1,5 @@
+// java 17
+
 public class PositiveNegativeorZero {
 
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class PositiveNegativeorZero {
     public static void checkNumber( int num ) {
 
         if (num>0) {
-            System.out.println("positive");
+            System.out.println("positive");  
         } else if (num<0) {
             System.out.println("negative");
         } else {
