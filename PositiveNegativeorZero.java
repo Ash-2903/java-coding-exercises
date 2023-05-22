26639f7c49c52d42fe7e4e71aa32aeba3d575692
Ashritha Herle <ashrithaherle2903@gmail.com>
2023-05-22 17:26:17 +0530

    Add files via upload

---
+++ PositiveNegativeorZero.java
@@ -0,0 +1,19 @@
+public class PositiveNegativeorZero {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void checkNumber( int num ) {
+
+        if (num>0) {
+            System.out.println("positive");
+        } else if (num<0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+      
+    } 
+
+}
